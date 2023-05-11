3946dd502b89614ded10f6019078bc12b34a4e4a
Hassan Tariq <128592715+hsuntariq@users.noreply.github.com>
2023-05-11 16:03:08 +0500

    Update audio.js

--- audio.js
+++ audio.js
@@ -8,6 +8,7 @@ const getAudio = async () => {
         const audioTag = document.createElement("AUDIO")
         const columns = document.createElement("div")
         columns.classList.add("col-lg-4")
+        columns.classList.add("layout")
         container.appendChild(columns)
         const title = document.createElement("p")
         title.textContent = item.name
